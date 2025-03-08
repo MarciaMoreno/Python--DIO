ea7066db6822915bfe27742cf5cc874d41a8d0ef
MarciaMoreno <mapat.soad@gmail.com>
2025-03-08 13:39:41 -0300

    Inserção de novos exercicios das aulas

---
+++ Teste das Aulas/Introducao_Linguagem_Python/Numerico_String.py
@@ -0,0 +1,9 @@
+preco = 10.58
+idade = 28
+
+print(str(preco))
+
+print(str(idade))
+
+texto = f"idade {idade} / preço R$ {preco}"
+print(texto)
---
+++ Teste das Aulas/Introducao_Linguagem_Python/Variaveis_Constantes.py
@@ -0,0 +1,9 @@
+idade = 39
+nome = 'Marcia'
+print(f'Meu nome é {nome} e eu tenho {idade} anos de idade.')
+
+idade , nome = 22 , 'Elisa'
+print(idade, nome)
+
+ESTADOS_BRTASILEIROS = ["SC", "SP", "DF", "GO"]
+print(ESTADOS_BRTASILEIROS)
